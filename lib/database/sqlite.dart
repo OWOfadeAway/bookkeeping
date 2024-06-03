7511c008f8c78a817e3104cf6f428907ace12e4b
root <1965452276@qq.com>
2024-06-03 14:53:48 +0800

    aaa

--- lib/database/sqlite.dart
+++ lib/database/sqlite.dart
@@ -1,5 +1,3 @@
-import 'dart:ffi';
-
 import 'package:sqflite/sqflite.dart';
 import 'package:path_provider/path_provider.dart';
 import 'dart:io' as io;
--- lib/pages/page/addSendItem.dart
+++ lib/pages/page/addSendItem.dart
@@ -14,7 +14,47 @@ class _AddSendItem extends State<AddSendItem>{
     return Scaffold(
         appBar:AppBar(
           title: Text('记一笔'),
-        )
+        ),
+        body: Column(
+            children: [
+              Container(
+                padding:const EdgeInsets.all(10.0),
+                child:  Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text("请选择消费类型:",style: TextStyle(
+                      fontSize: 24.0,
+                      color: Colors.blue,
+                      fontStyle: FontStyle.italic,
+                      fontWeight: FontWeight.w600,
+                    ),),
+                    Container(
+                      width: MediaQuery.of(context).size.width,
+                      height: 150,
+                      decoration: BoxDecoration(
+                        color: Colors.amber,
+                        borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                      ),
+                      child: GridView.builder(
+                          itemCount:5,
+                          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                        //横轴元素个数
+                          crossAxisCount: 4,
+                          // //纵轴间距
+                          // mainAxisSpacing: ScreenUtil().setHeight(10),
+                          // // 横轴间距
+                          // crossAxisSpacing: ScreenUtil().setWidth(10),
+                          //子组件宽高长度比例
+                          childAspectRatio: 1), itemBuilder: (context, index) {
+                        print(index);
+                              return Text("123");
+                      }),
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
     );
   }
 
